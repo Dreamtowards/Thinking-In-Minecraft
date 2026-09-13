24a771ed836069e51e0455f2be386e1e97f8bf72
Dreamtowards <x@elytra.dev>
2026-09-13 11:55:37 +0800

    opt sidebar index to group

--- next.config.mjs
+++ next.config.mjs
@@ -9,10 +9,6 @@ const config = {
   reactStrictMode: true,
   async redirects() {
     return [
-      { source: '/history', destination: '/history/business', permanent: false },
-      { source: '/design', destination: '/design/constraint', permanent: false },
-      { source: '/foundations', destination: '/design/foundations', permanent: true },
-      { source: '/foundations/', destination: '/design/foundations', permanent: true },
     ];
   },
 };
