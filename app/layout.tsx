15297c95ce496fc59c41b7cc3ee3e7a32ec90436
Dreamtowards <x@elytra.dev>
2026-09-13 19:43:58 +0800

    opt: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export const metadata: Metadata = {
     template: `%s | ${appName}`,
   },
   description: appDescription,
+  icons: {
+    icon: '/favicon.png',
+  },
 };
 
 export default function Layout({ children }: LayoutProps<'/'>) {
