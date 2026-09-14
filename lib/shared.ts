bde173b6b1b0a2b354937948b350b2b81ac09939
Dreamtowards <x@elytra.dev>
2026-09-15 01:15:08 +0800

    opt: toc desc

--- lib/shared.ts
+++ lib/shared.ts
@@ -1,13 +1,13 @@
-export const appName = 'Minecraft 設計思想';
+export const appName = 'Minecraft 设计思想';
 export const appDescription =
-  '歷史、設計與演算法 · Thinking in Minecraft: History, Designs and Algorithms';
+  '历史、设计与演算法 · Thinking in Minecraft: History, Designs and Algorithms';
 export const docsRoute = '/';
 export const docsImageRoute = '/og/docs';
 export const docsContentRoute = '/llms.mdx/docs';
 
 export const gitConfig = {
   user: 'Dreamtowards',
-  repo: 'Thinking-In-Minecraft',
+  repo: 'Thinking-in-Minecraft',
   branch: 'main',
 };
 
